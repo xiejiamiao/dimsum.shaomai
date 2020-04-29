a98d3c712ab1fb4d5347ae679cb865c5c4e1b56e
Jiamiao.x <xiejiamiao0326@hotmail.com>
2020-04-29 19:02:42 +0800

    nothing

---
+++ Dimsum.Shaomai.Manager/Controllers/SolutionController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dimsum.Shaomai.Manager.Controllers
+{
+    [Authorize]
+    [Route("[controller]/[action]")]
+    public class SolutionController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddSolution()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task AddSolutionHandler(string solutionName)
+        {
+            
+        }
+
+        public async Task<IActionResult> Worker()
+        {
+            return View();
+        }
+    }
+}
