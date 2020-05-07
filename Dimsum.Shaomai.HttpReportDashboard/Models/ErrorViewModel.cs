dbcfbe66d44763417691c408c0909081a0906cf5
Jiamiao.x <xiejiamiao0326@hotmail.com>
2020-05-07 16:05:48 +0800

    nothing

---
+++ Dimsum.Shaomai.HttpReportDashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dimsum.Shaomai.HttpReportDashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- Dimsum.Shaomai.Util/ConstValue.cs
+++ Dimsum.Shaomai.Util/ConstValue.cs
@@ -8,7 +8,7 @@ namespace Dimsum.Shaomai.Util
     {
         public static List<string> SafeColor=new List<string>()
         {
-            "#b8def5","#d0ebf2","#eddff3","#CC66FF","#FF9999","#FFFFCC"
+            "#bfdef7","#fff4bd","#ffdae2","#bff2ea","#ebd6ff"
         };
     }
 }
